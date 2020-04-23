2a3e3e598296ec8a42567c29eed0b068687746a8
gaurav <sharma02gaurav@gmail.com>
2020-04-23 22:24:10 +0530

    eslint fix

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,6 @@
 /**
  * this expression will auto deploy all the routes in this path
  */
-export default app => {
+export default (app) => {
 	app.get('/baremetal', (req, res) => res.send('Hello Bare Metal Express Server'));
-}
+};
